d4b6a489b72d5a1cd707c35809c46e043a685543
unknown <tdashpute@gmail.com>
2020-11-30 17:23:46 +0530

    terraform files are uploaded

---
+++ RDS/provider.tf
@@ -0,0 +1,7 @@
+# provider.tf
+provider "aws" {
+  version    = "~> 2.22.0"
+  region     = "us-east-1"
+  #access_key = "AKIAZVIYFOTWIYPSELNN"
+  #secret_key = "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"
+}
